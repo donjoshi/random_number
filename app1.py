dbe5109dd5dab710852317b0404fce5e42f1bf11
Don Joshi <donjneelamkavil@gmail.com>
2022-09-28 17:42:57 +0530

    first-commit

---
+++ app1.py
@@ -0,0 +1,27 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+import random
+
+kivy.require('1.9.0')
+
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.random_label.text = str(random.randint(1, 100))
+
+
+class NeuralRandom(App):
+
+    def build(self):
+        return MyRoot()
+
+
+neuralRandom = NeuralRandom()
+neuralRandom.run()
